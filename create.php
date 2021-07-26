1ffff2467c410cd7d35b4243a998232c336db58e
jshgold <jshgold@naver.com>
2021-07-26 19:12:21 +0900

    study_0726
    php

--- create.php
+++ create.php
@@ -7,15 +7,7 @@
             echo "Home~!";
         }
     }
-    function print_description(){
-        if(isset($_GET['id'])){
-            echo $file = file_get_contents("data/".$_GET['id'],true);
-        }
-        else
-        {
-            echo "Welcome To My Homepage~!";
-        }
-    }
+    
     function load_to_file(){
         $list=scandir("./data");
         #var_dump($list);
@@ -52,29 +44,14 @@
         load_to_file();
         ?>
     </ol>
-    <a href="create.php">Create</a>
-    <?php
-    if(isset($_GET['id'])){?>
-        <a href="update.php?id=<?=$_GET['id'];?>">Update</a>
-    <?php
-    }
-    ?>
-
-
-    <h2>
-    <?php
-    print_title();
-    ?>
-    </h2>
     
-    <?php
-    print_description();
-    ?>
+    <form action="create_process.php" method="post">
+        <p><input type="text" name="title" placeholder="Title"></p>
+        <p><textarea name="description" placeholder="Description"></textarea></p>
+        <p><input type="submit"></p>
+
+    </form>
+
 </body>
 
 </html>
-
-
-
-
-
