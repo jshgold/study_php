4aa9b52e3cf81cbb05de2ccb57df79ea4f3a921f
jshgold <jshgold@naver.com>
2021-07-27 18:19:26 +0900

    0727_PHP_Study
    delete

--- home.php
+++ home.php
@@ -56,6 +56,11 @@
     <?php
     if(isset($_GET['id'])){?>
         <a href="update.php?id=<?=$_GET['id'];?>">Update</a>
+        
+        <form action="delete_process.php" method="post">
+            <input type="hidden" name="id" value="<?=$_GET['id']?>">
+            <input type="submit" value="delete">
+        </form>
     <?php
     }
     ?>
